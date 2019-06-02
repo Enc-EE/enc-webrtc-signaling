0eac68a5820b0297440cc2181bcb3f549196f7ba
Eric Eichhorn <ee.eric.eichhorn@outlook.de>
2019-06-02 23:07:17 +0200

    azure ts node test

--- dist/models/connectionWorkflowClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ConnectionWorkflowClient = /** @class */ (function () {
-    function ConnectionWorkflowClient() {
-        this.candidates = [];
-    }
-    return ConnectionWorkflowClient;
-}());
-exports.ConnectionWorkflowClient = ConnectionWorkflowClient;
